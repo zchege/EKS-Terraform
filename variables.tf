041f30a9f4adf8beb82e88a44ec50ba165b8e233
zchege <zachqy@gmail.com>
2025-08-24 20:49:19 +0300

    modified key

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "aws-ec2-keypair"
+  default     = "DevOps-Shack"
 }
 
